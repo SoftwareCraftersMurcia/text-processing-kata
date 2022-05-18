1924bdb81cf0aee46d9270b203679df26cc4f965
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-18 16:34:22 +0200

    chore: refactoring the production code

--- php/src/TextProcessor.php
+++ php/src/TextProcessor.php
@@ -5,21 +5,38 @@ namespace Kata;
 
 class TextProcessor
 {
-    public static function analyse(string $string): ProcessedText
+    private const WORD_SEPARATOR = ' ';
+    private const EMPTY_STRING = '';
+    private const TOP_WORDS_LIMIT = 10;
+    private const TOP_WORD_STARTING = 0;
+    private const INVALID_CHARACTERS = ['.', ','];
+
+    public static function analyse(string $blogPost): ProcessedText
     {
-        if (empty($string)) {
+        if (empty($blogPost)) {
             return ProcessedText::empty();
         }
 
-        $string = str_replace(['.', ','], '', $string);
-        $listOfWords = explode(' ', strtolower($string));
-
-        $words = array_count_values($listOfWords);
-        uasort($words, fn(int $a, int $b) => $b <=> $a);
+        $listOfWords = self::cleanListOfWords($blogPost);
+        $orderTopTenWords = self::orderedTopTenWords($listOfWords);
+        $totalWords = count($listOfWords);
 
         return new ProcessedText(
-            array_keys(array_slice($words, 0, 10)),
-            count($listOfWords)
+            $orderTopTenWords,
+            $totalWords
         );
+    }
+
+    private static function cleanListOfWords(string $string): array
+    {
+        $string = str_replace(self::INVALID_CHARACTERS, self::EMPTY_STRING, $string);
+        return explode(self::WORD_SEPARATOR, strtolower($string));
+    }
+
+    public static function orderedTopTenWords(array $listOfWords): array
+    {
+        $words = array_count_values($listOfWords);
+        uasort($words, fn(int $a, int $b) => $b <=> $a);
+        return array_keys(array_slice($words, self::TOP_WORD_STARTING, self::TOP_WORDS_LIMIT));
     }
 }
