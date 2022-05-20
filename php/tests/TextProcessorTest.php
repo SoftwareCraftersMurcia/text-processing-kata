382779d6f52b38ed8a5136f922a976301cc129b5
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-20 15:34:33 +0200

    chore: refactoring the test

--- php/tests/TextProcessorTest.php
+++ php/tests/TextProcessorTest.php
@@ -9,23 +9,47 @@ class TextProcessorTest extends TestCase
 {
     public function provideTexts(): iterable
     {
-        yield ['', [], 0];
-        yield ['one_word', ['one_word'], 1];
-        yield ['one_word two_words', ['one_word', 'two_words'], 2];
-        yield ['one_word two_words one_word', ['one_word', 'two_words'], 3];
-        yield ['one_word two_words ONE_WORD', ['one_word', 'two_words'], 3];
-        yield ['Hello, this is an example for you to practice. You should grab this text and make it as your test case.', ['this', 'you', 'hello', 'is', 'an', 'example', 'for', 'to', 'practice', 'should'], 21];
+        yield [
+            '$input' => '',
+            '$expectedWords' => [],
+            '$totalWords' => 0
+        ];
+        yield [
+            '$input' => 'one_word',
+            '$expectedWords' => ['one_word'],
+            '$totalWords' => 1
+        ];
+        yield [
+            '$input' => 'one_word two_words',
+            '$expectedWords' => ['one_word', 'two_words'],
+            '$totalWords' => 2
+        ];
+        yield [
+            '$input' => 'one_word two_words one_word',
+            '$expectedWords' => ['one_word', 'two_words'],
+            '$totalWords' => 3
+        ];
+        yield [
+            '$input' => 'one_word two_words ONE_WORD',
+            '$expectedWords' => ['one_word', 'two_words'],
+            '$totalWords' => 3
+        ];
+        yield [
+            '$input' => 'Hello, this is an example for you to practice. You should grab this text and make it as your test case.',
+            '$expectedWords' => ['this', 'you', 'hello', 'is', 'an', 'example', 'for', 'to', 'practice', 'should'],
+            '$totalWords' => 21
+        ];
     }
 
     /**
      * @test
      * @dataProvider provideTexts
      */
-    public function given_a_text_to_process(string $input, array $expectedWords, int $totalwords): void
+    public function given_a_text_to_process(string $input, array $expectedWords, int $totalWords): void
     {
         $processedText = TextProcessor::analyse($input);
 
         self::assertSame($expectedWords, $processedText->topWords());
-        self::assertSame($totalwords, $processedText->totalWords());
+        self::assertSame($totalWords, $processedText->totalWords());
     }
 }
