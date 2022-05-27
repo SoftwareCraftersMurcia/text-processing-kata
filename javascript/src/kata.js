451411e1a2f0f9a7c86c946a62a0aa081bf7d7ba
Carlos Castillo <castillo.st@gmail.com>
2022-05-27 19:22:10 +0200

    Refactor: move count logic to a separate function

--- javascript/src/kata.js
+++ javascript/src/kata.js
@@ -1,6 +1,7 @@
+const countWords = (phrase) => phrase.split(' ').length;
+
 const processText = (phrase) => {
-  const counter = phrase.split(' ')
-    return `The text has in total ${counter.length} words`;
+  return `The text has in total ${countWords(phrase)} words`;
 };
 
 module.exports = {
